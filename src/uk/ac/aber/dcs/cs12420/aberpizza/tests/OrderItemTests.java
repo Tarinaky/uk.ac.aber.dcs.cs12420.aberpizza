1a806119e4380b70a2b2dcbff253a2e6deaa2fb2
Tarinaky <Tarinaky@gmail.com>
2012-03-28 02:54:27 +0100

    Added unit testCase for OrderItems.

---
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/tests/OrderItemTests.java
@@ -0,0 +1,13 @@
+package uk.ac.aber.dcs.cs12420.aberpizza.tests;
+
+import org.junit.Before;
+
+import uk.ac.aber.dcs.cs12420.aberpizza.data.OrderItem;
+import junit.framework.TestCase;
+
+public class OrderItemTests extends TestCase {
+	private OrderItem fixture = null;
+	
+	@Before
+	public void setUp() {}
+}
