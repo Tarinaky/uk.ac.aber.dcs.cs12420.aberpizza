2642458dacd1d637d66901f54729bfdb14e46021
Tarinaky <Tarinaky@gmail.com>
2012-03-28 23:45:09 +0100

    Writen unit tests for TillTests.

--- src/uk/ac/aber/dcs/cs12420/aberpizza/tests/TillTests.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/tests/TillTests.java
@@ -1,7 +1,11 @@
 package uk.ac.aber.dcs.cs12420.aberpizza.tests;
 
-import org.junit.Before;
+import java.math.BigDecimal;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.aber.dcs.cs12420.aberpizza.data.Order;
 import uk.ac.aber.dcs.cs12420.aberpizza.data.Till;
 import junit.framework.TestCase;
 
@@ -12,4 +16,21 @@ public class TillTests extends TestCase {
 	public void setUp() {
 		fixture = new Till();
 	}
+	
+	@Test
+	public void testAddOrder() {
+		Order testOrder = new Order();
+		
+		testOrder.addItem(new TestItem(), 1);
+		assertTrue("Subtotal should be 10.99; received "+testOrder.getSubtotal().toString(),
+				testOrder.getSubtotal().compareTo(new BigDecimal(""+10.99) ) == 0);
+		
+		fixture.addOrder(testOrder);
+		assertTrue("Total for day should be 10.99; received "+fixture.getTotalForDay().toString(),
+				fixture.getTotalForDay().compareTo(new BigDecimal(""+10.99) ) == 0);
+
+		
+	}
+	
+	//TODO: Test serialisation.
 }
