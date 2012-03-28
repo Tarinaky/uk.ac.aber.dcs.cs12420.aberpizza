4522dea6558fc10f31919cf8903327d7ce187cc2
Tarinaky <Tarinaky@gmail.com>
2012-03-28 03:37:12 +0100

    Added javadoc information for OrderItemTests.

--- src/uk/ac/aber/dcs/cs12420/aberpizza/tests/OrderItemTests.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/tests/OrderItemTests.java
@@ -12,7 +12,7 @@ import junit.framework.TestCase;
 /**
  * Test item describing a simple implementation of an Item.
  * 
- * @author Tarinaky
+ * @author Jacob Smith
  *
  */
 class TestItem implements Item {
@@ -46,17 +46,26 @@ class TestItem implements Item {
 public class OrderItemTests extends TestCase {
 	private OrderItem fixture = null;
 	
+	/**
+	 * Initialise a test fixture containing 3 simple Items each priced at £10.99.
+	 */
 	@Before
 	public void setUp() {
 		fixture = new OrderItem(new TestItem(), 3);
 	}
 	
+	/**
+	 * Tests the OrderItem.getQuantity() method.
+	 */
 	@Test
 	public void testQuantity() {
 		assertTrue("Initial quantity is expected to be 3; received"+fixture.getQuantity(),
 				fixture.getQuantity() == 3);
 	}
 	
+	/**
+	 * Tests the OrderItem.getOrderItemTotal() method.
+	 */
 	@Test
 	public void testTotal() {
 		assertTrue("Expected total for 3 items at 10.99 is 32.97; received "+fixture.getOrderItemTotal().toString(), 
