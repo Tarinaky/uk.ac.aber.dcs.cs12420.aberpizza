000cb30c984953adcc89f257487b43b2853c5abb
Tarinaky <Tarinaky@gmail.com>
2012-03-28 03:51:01 +0100

    Added tests to OrderTests.

--- src/uk/ac/aber/dcs/cs12420/aberpizza/tests/OrderTests.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/tests/OrderTests.java
@@ -1,6 +1,9 @@
 package uk.ac.aber.dcs.cs12420.aberpizza.tests;
 
+import java.math.BigDecimal;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import uk.ac.aber.dcs.cs12420.aberpizza.data.Order;
 import junit.framework.TestCase;
@@ -12,4 +15,27 @@ public class OrderTests extends TestCase {
 	public void setUp() {
 		fixture = new Order();
 	}
+	
+	@Test
+	public void testCustomerName() {
+		fixture.setCustomerName("Test string");
+		assertTrue("Expected 'Test string'; received "+fixture.getCustomerName(),
+				fixture.getCustomerName() == "Test string");
+	}
+	
+	@Test
+	public void testRemoveItems() {
+		TestItem item = new TestItem();
+		
+		fixture.addItem(item, 1);
+		fixture.updateItemQuantity(item, 3);
+		fixture.updateItemQuantity(item, 0);
+		
+		assertTrue("Expected subtotal to be 0; received "+fixture.getSubtotal(),
+				fixture.getSubtotal().compareTo(new BigDecimal(""+0) )== 0 );
+	}
+	
+	//TODO: Test getSubtotal():BigDecimal
+	//TODO: Test getDiscount():BigDecimal
+	//TODO: Test getReceipt():String
 }
