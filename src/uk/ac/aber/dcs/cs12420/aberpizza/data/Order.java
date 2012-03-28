2d4b29dac828a6fd71b957cd70aa3ec3e8356348
Tarinaky <Tarinaky@gmail.com>
2012-03-28 21:53:55 +0100

    Added orderTable parameter to Order.

--- src/uk/ac/aber/dcs/cs12420/aberpizza/data/Order.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/data/Order.java
@@ -2,6 +2,8 @@ package uk.ac.aber.dcs.cs12420.aberpizza.data;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * A list of {@link OrderItem} objects. There are methods
@@ -12,14 +14,29 @@ import java.util.Date;
  * return a string, plain text or html.
  * 
  * @author Neil Taylor
- *
+ * @author Jacob Smith
  */
 public class Order {
 	
-	private Date date;
-	private String customerName;
+	/**
+	 * The date the order was created. This is automatically generated
+	 * when the order is constructed to match today's date. This field
+	 * is used to sort and filter the orders when they are complete and stored.
+	 */
+	private Date date = null;
+	private String customerName = null;
 	
-	public Order() {}
+	/**
+	 * A self-balancing tree containing each row of the order
+	 * ordered according to the item. This can be thought of as
+	 * a table where each item may appear once (or not at all).
+	 */
+	private Map<Item,OrderItem> orderTable = null;
+	
+	public Order() {
+		orderTable = new TreeMap<Item,OrderItem>();
+		date = new Date();
+	}
 	
 	public void setCustomerName(String name) {
 		this.customerName = name;
