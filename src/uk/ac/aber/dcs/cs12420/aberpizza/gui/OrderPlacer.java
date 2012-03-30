daf74a41e8a5a9c61feea0ddc173966ea0db02ab
Tarinaky <tarinaky@gmail.com>
2012-03-30 07:04:00 +0100

    Added OrderPlacer.java

---
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/gui/OrderPlacer.java
@@ -0,0 +1,21 @@
+package uk.ac.aber.dcs.cs12420.aberpizza.gui;
+
+import uk.ac.aber.dcs.cs12420.aberpizza.data.Till;
+
+/**
+ * Creates a JFrame within which orders may be created and placed.
+ * 
+ * @author Jacob Smith
+ *
+ */
+public class OrderPlacer {
+	
+	/**
+	 * Create, arrange and set visible the elements of the JFrame.
+	 * 
+	 * @param till The till model that this JFrame should operate upon.
+	 */
+	public OrderPlacer() {}
+	
+
+}
