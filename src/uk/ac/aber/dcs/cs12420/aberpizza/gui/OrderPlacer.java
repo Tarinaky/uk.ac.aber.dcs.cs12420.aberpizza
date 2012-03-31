3381fc97c3f132fee079f2a93074e32241e39002
Tarinaky <tarinaky@gmail.com>
2012-03-31 12:26:03 +0100

    Added some GUI widgets to OrderPlacer.

--- src/uk/ac/aber/dcs/cs12420/aberpizza/gui/OrderPlacer.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/gui/OrderPlacer.java
@@ -1,5 +1,13 @@
 package uk.ac.aber.dcs.cs12420.aberpizza.gui;
 
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+
 import uk.ac.aber.dcs.cs12420.aberpizza.data.Till;
 
 /**
@@ -21,7 +29,35 @@ public class OrderPlacer {
 	 */
 	public OrderPlacer(Till till) {
 		this.till = till;
+		
+		JFrame frame = new JFrame("Order Placer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel layout = new JPanel();
+		frame.getContentPane().add(layout);
+		
+		
+		JToolBar toolbar = new JToolBar("toolbar");
+		layout.add(toolbar);
+		
+		JButton button = null;
+		
+		button = new JButton("New order");
+		toolbar.add(button);
+		
+		button = new JButton("Send order");
+		toolbar.add(button);
+		
+		JPanel orderPanel = new JPanel();
+		orderPanel.setSize(640, 480);
+		layout.add(orderPanel);
+		
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
+	public static void main (String[] args) { OrderPlacer foo = new OrderPlacer(new Till()); }
+	
 
+	
 }
