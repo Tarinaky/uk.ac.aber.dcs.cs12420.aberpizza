8b4fd8d6dec85b542a1702992ecd63afc6e408db
Tarinaky <tarinaky@gmail.com>
2012-03-31 14:19:39 +0100

    Added layout to rhs o OrderBuilder.

--- src/uk/ac/aber/dcs/cs12420/aberpizza/gui/OrderBuilder.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/gui/OrderBuilder.java
@@ -2,20 +2,25 @@ package uk.ac.aber.dcs.cs12420.aberpizza.gui;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 
+import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 
 import uk.ac.aber.dcs.cs12420.aberpizza.data.Order;
 
 public class OrderBuilder {
 	private Order order = null;
-	private Component panel = null;
+	private JSplitPane panel = null;
 	
 	public OrderBuilder () {
 		newOrder();
 		
 		panel = new JSplitPane();
 		panel.setMinimumSize(new Dimension(100,50));
+		
+		JPanel right = new JPanel(new GridBagLayout() );
+		panel.setRightComponent(right);
 		
 		
 	}
