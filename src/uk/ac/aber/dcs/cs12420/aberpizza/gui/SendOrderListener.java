0809cb8dde900c1584cb33cceb644f0b44d11f53
Tarinaky <Tarinaky@gmail.com>
2012-04-06 09:14:53 +0100

    Customer name requested and saved with the order.

--- src/uk/ac/aber/dcs/cs12420/aberpizza/gui/SendOrderListener.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/gui/SendOrderListener.java
@@ -3,6 +3,8 @@ package uk.ac.aber.dcs.cs12420.aberpizza.gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 public class SendOrderListener implements ActionListener {
 
 	private OrderPlacer view = null;
@@ -13,6 +15,10 @@ public class SendOrderListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		String customerName = (String) JOptionPane.showInputDialog("Customer name..."); 
+		
+		view.order().getOrder().setCustomerName(customerName);
+		
 		view.getTill().addOrder(view.order().getOrder());
 		view.order().newOrder();
 
