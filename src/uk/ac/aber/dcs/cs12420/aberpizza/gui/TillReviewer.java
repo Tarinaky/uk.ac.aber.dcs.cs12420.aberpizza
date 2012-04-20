61528967abc7e52b0e0bf9336fa35ad6b6d2b5c2
Tarinaky <Tarinaky@gmail.com>
2012-04-20 16:27:07 +0100

    Reviewer now lists customer names and value as button. Todo: make button display receipt.

--- src/uk/ac/aber/dcs/cs12420/aberpizza/gui/TillReviewer.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/gui/TillReviewer.java
@@ -1,16 +1,25 @@
 package uk.ac.aber.dcs.cs12420.aberpizza.gui;
 
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
 
+import uk.ac.aber.dcs.cs12420.aberpizza.data.Order;
 import uk.ac.aber.dcs.cs12420.aberpizza.data.Till;
 
 public class TillReviewer {
 	private JFrame frame = null;
 	private Till till = null;
+	private JPanel pane = null;
 
 	public TillReviewer(Till till) {
 		this.till = till;
@@ -33,9 +42,58 @@ public class TillReviewer {
 			
 		});
 		
+		addElements();
+		
 		frame.pack();
 		frame.setSize(800,480);
 		frame.setVisible(true);
+		
+	}
+
+	public void addElements() {
+		Container layout = frame.getContentPane();
+		GridBagLayout gbl = new GridBagLayout();
+		layout.setLayout(gbl);
+		gbl.layoutContainer(layout);
+		GridBagConstraints c = new GridBagConstraints();
+		
+		JToolBar toolbar = new JToolBar("toolbar");
+		toolbar.setFloatable(false);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 1;
+		c.weighty = 0;
+		layout.add(toolbar, c);
+		
+		JButton button = new JButton("Open file...");
+		toolbar.add(button);
+		
+		pane = new JPanel();
+		pane.setLayout(new GridBagLayout() );
+		drawOrders();
+		
+		c.gridy = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		layout.add(new JScrollPane(pane),c);
+		
+	}
+
+	private void drawOrders() {
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 1;
+		c.weighty = 0;
+		
+		for (Order order : till.getOrders() ) {
+			c.gridx = 0;
+			JButton button = new JButton(""+order.getCustomerName()+", £"+order.getSubtotal() );
+			pane.add(button,c);
+			c.gridy++;
+		}
 		
 	}
 
