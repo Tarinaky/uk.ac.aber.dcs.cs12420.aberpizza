0416168aa934bea87fbe382075a89af86b001d6d
Tarinaky <Tarinaky@gmail.com>
2012-04-25 17:12:00 +0100

    Inventory now documented.

--- src/uk/ac/aber/dcs/cs12420/aberpizza/data/Inventory.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/data/Inventory.java
@@ -8,6 +8,9 @@ import java.util.*;
 /**
  * Class for storing Item objects representing goods for sale.
  * 
+ * The Inventory loaded into the program represents the current Inventory for sale. Past products
+ * are individually saved in a Till's log. While this bloats the log somewhat it reduces dependencies.
+ * 
  * @author Jacob Smith
  *
  */
@@ -16,21 +19,41 @@ public class Inventory {
 	 * A collection of Items sold by this store.
 	 */
 	private ArrayList<Item> items = null;
+	/**
+	 * A collection of Discounts to be applied to items made at this store. Discounts will be applied
+	 * in whatever order they appear in this Collection.
+	 */
 	private ArrayList<Discount> discounts = null;
-	
+	/**
+	 * A handle to the last inventory deserialised by {@link #load(String)}. More than one Inventory
+	 * should not be in use at a time without removing this field and refactoring appropriately.
+	 */
 	public static Inventory singleton = null;
 	
 	public Inventory() {
 		items = new ArrayList<Item>();
 		discounts = new ArrayList<Discount>();
 	}
-	
+	/** @see #items */
 	public ArrayList<Item> getItems() { return items; }
+	/** @see #items */
 	public void setItems(ArrayList<Item> items) { this.items = items; }
 	
+	/** @see #discounts */
 	public ArrayList<Discount> getDiscounts() { return discounts; }
+	/** @see #discounts */
 	public void setDiscounts(ArrayList<Discount> discounts) { this.discounts = discounts; }
 	
+	/**
+	 * Serialise this inventory object to disk. The file will be written in the data directory,
+	 * relative to whatever current working directory the JVM is invoked with.
+	 * 
+	 * This method is not used by the AberPizza program 
+	 * ({@link uk.ac.aber.dcs.cs12420.aberpizza.gui.OrderPlacer#main(String[])}) but
+	 * is used by {@link uk.ac.aber.dcs.cs12420.aberpizza.pipeline.InventoryEditor#main(String[])} 
+	 * @param filename Name of the file the inventory should be serialised to.
+	 * @throws IOException
+	 */
 	public void save(String filename) throws IOException {
 		if (filename == null) {
 			filename = "inventory.xml";
@@ -51,6 +74,15 @@ public class Inventory {
 		
 	}
 	
+	/**
+	 * Deserialises an Inventory object from the filename specified. The method first attempts
+	 * to find the file inside the JAR this class is run from (inside a data directory at said JAR's
+	 * root) or fails onto searching data in the current working directory.
+	 * @param filename The name of the file to be loaded as an Inventory, or null to default to
+	 * 'inventory.xml'
+	 * @return The Inventory object. This object will also be referenced by {@link #singleton}.
+	 * @throws IOException
+	 */
 	public static Inventory load(String filename)
 	throws IOException {
 		if (filename == null) {
