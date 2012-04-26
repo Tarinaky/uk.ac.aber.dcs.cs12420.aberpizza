c7504536f0fbf647add2684d96288e6131b2a2c1
Tarinaky <Tarinaky@gmail.com>
2012-04-26 19:22:52 +0100

    Class diagram worked on.

--- writeup/data_class_diagram.java
+++ writeup/data_class_diagram.java
@@ -1,5 +1,5 @@
 /**
- * @hide
+ * @opt hide
  * @author Tarinaky
  *
  */
@@ -11,12 +11,37 @@ class BigDecimal {}
  *
  */
 interface Item {
-	String getDescription();
-	BigDecimal getPrice();
-	double getSize();
-	String getStringPrice();
-	void setDescription(String s);
-	void setPrice(BigDecimal price);
-	void setSize(double d);
-	setStringPrice(String s);
+	public String getDescription();
+	public BigDecimal getPrice();
+	public double getSize();
+	public String getStringPrice();
+	public void setDescription(String s);
+	public void setPrice(BigDecimal price);
+	public void setSize(double d);
+	public void setStringPrice(String s);
 }
+/**
+ * @opt all
+ * @assoc 1 - * Item
+ * @assoc 1 - * Discount
+ * @author Tarinaky
+ *
+ */
+class Inventory {
+	private ArrayList<Discount> discounts;
+	private ArrayList<Item> items;
+	public static Inventory singleton;
+	
+	public ArrayList<Discount> getDiscounts() {}
+	public ArrayList<Item> getItems() {}
+	public static Inventory load(String filename) {}
+	public void save(String filename) {}
+	public void setDiscounts(ArrayList<Discount> discounts) {}
+	public void setItems(ArrayList<Item> items) {}
+}
+/** @opt hide */
+class ArrayList<Discount> {}
+/** @opt hide */
+class ArrayList<Item> {}
+
+interface Discount {}
